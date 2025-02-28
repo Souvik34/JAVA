ef2b87951736fb68e813a30e8998b9bbc548171c
Souvik <souvik13109@gmail.com>
2025-02-28 23:49:17 +0530

    Refactor print.java to include a main method and add comments for clarity

--- Preparation/print.java
+++ Preparation/print.java
@@ -1,4 +1,18 @@
 package Preparation;
-public class print {
-    
+// System.out.print();
+
+// ln-> next Line
+// print -> Same line
+
+
+// public -> keyword. 
+
+public class print
+{
+  public static void main(String args[]) // void print(); void display()
+  {
+    System.out.println("Hello World!");
+  }
 }
+
+
